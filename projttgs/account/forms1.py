eabd38aba7d8dda3675ec9f07df03dda788c276b
Ankit <ankitymca27@gmail.com>
2026-04-26 21:28:36 +0530

    password username , and live demo fixed

--- projttgs/account/forms1.py
+++ projttgs/account/forms1.py
@@ -1,10 +1,11 @@
+import re
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
@@ -18,9 +19,20 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'email')
 
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if password:
+            pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_\-#]).{8,}$'
+            if not re.match(pattern, password):
+                raise forms.ValidationError(
+                    'Password must be 8+ characters and include an uppercase letter, '
+                    'a lowercase letter, a digit, and a special character (@$!%*?&_-#).'
+                )
+        return password
+
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        if cd.get('password') != cd.get('password2'):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
